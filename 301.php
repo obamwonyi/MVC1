2e6ae31518261774efa9c673c4c02bff85c8130c
obamwonyi <developer.oduwa@gmail.com>
2022-05-06 12:45:46 -0400

    added the index.php root file

---
+++ 301.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html> 
+<head>
+
+</head>
+
+<body>
+    <p>a 301 error page redirect . </p>
+</body>
+
+</html>
---
+++ 404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html> 
+<head>
+
+</head>
+
+<body>
+    <p>a 404 error has occured.</p>
+</body>
+
+</html>
