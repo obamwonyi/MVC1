5f7cc240010929d36dcea39dc3f3dd902e131322
obamwonyi <developer.oduwa@gmail.com>
2022-05-12 23:31:10 +0100

    added a constructor to the App class

--- app/app.php
+++ app/app.php
@@ -4,12 +4,15 @@ namespace App\app;
 //the app class 
 interface App 
 {
-
+    
 }
 
 class Appication implements App 
 {
-
+    public function __construct() 
+    {
+        //data to preset .
+    }
 }
 
 
