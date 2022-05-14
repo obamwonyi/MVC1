ae5939460f2a3dded9bb8682086019bcda0d0c9c
obamwonyi <developer.oduwa@gmail.com>
2022-05-14 13:21:23 -0400

    added composer for dependency management

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '52aaf2e4f9cc6cdd8d923ea8df65ac71e1d302a6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '52aaf2e4f9cc6cdd8d923ea8df65ac71e1d302a6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
