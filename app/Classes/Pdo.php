614de77e29e71600269c3322d0fe82dd39a6ca6b
obamwonyi <developer.oduwa@gmail.com>
2022-05-16 16:56:56 -0400

    predefine the parameters needed for the db connection

--- app/Classes/Pdo.php
+++ app/Classes/Pdo.php
@@ -4,10 +4,23 @@ namespace App\Classess;
 
 class  Pdo 
 {
-    public function __construct()
+
+    //preseting the database at the moment of instantiation 
+    public function __construct(string $dbhost,string $dbuser,string $dbname,string $dbpass)
     {
+        //setting the database host 
+        define("DB_HOST",$dbhost);
+        //setting the database user
+        define("DB_USER",$dbuser) ;
+        //setting the database name
+        define("DB_NAME",$dbname); 
+        //setting the database pass  
+        define("DB_PASS",$dbpass); 
+
         
     }
+
+    
 }
  
 
